fbfd7caeaa369bad428d4aaaeb52502386a0a813
Gilberto de Melo <gilbertogdemelo@hotmail.com>
2025-04-26 13:15:26 +0200

    Added parameters to change size of icons and glows

--- LogTimelineConfig.lua
+++ LogTimelineConfig.lua
@@ -5,6 +5,8 @@ OVERLAP_DISTANCE = 15
 PHASE_SPEED = 2.5
 MAX_ALPHA = 1.0
 MIN_ALPHA = 0.0
+ICON_SIZE = 32
+GLOW_SIZE_ICON_PERCENTAGE = 20 -- how bigger is the glow as compared to the icon size
 
 -- Logarithmic Scale Settings
 LOGARITHMIC_SCALE = true
